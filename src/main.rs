602ba966416075fbebdae3aa08ac6e13669c3846
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-05-04 11:08:44 +0100

    Read content function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let default_posts_path = "_posts";
+    let posts = read_content(default_posts_path);
+    println!("{:?}", posts);
+}
+
+fn read_content(source_directory: &str) -> HashMap<String, String> {
+    let mut posts = HashMap::new();
+
+    let files = std::fs::read_dir(source_directory).unwrap();
+    for file in files {
+        let file_path = file.unwrap().path();
+        let file_name = file_path.file_name().unwrap().to_str().unwrap();
+        let file_content = std::fs::read_to_string(&file_path).unwrap();
+        posts.insert(file_name.to_string(), file_content);
+    }
+
+    posts
 }
