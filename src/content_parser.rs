ecd3c47fa263bdd93f5e3835008082c3a0c3cdd0
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-05-08 03:54:51 +0100

    Add metadata parsing

--- src/content_parser.rs
+++ src/content_parser.rs
@@ -1,4 +1,5 @@
-use crate::entities::{ContentKind, RawPost};
+use crate::entities::{ContentKind, Metadata, RawPost};
+use chrono::NaiveDate;
 use std::collections::HashMap;
 
 fn parse_front_matter(lines: Vec<String>) -> Result<HashMap<String, String>, String> {
@@ -50,6 +51,32 @@ pub fn parse_content(og_path: &str, content: &str) -> Result<RawPost, String> {
         body: body.join("\n"),
         og_path: og_path.to_string(),
         kind: ContentKind::Markdown, // TODO: Currently only markdown is supported
+    })
+}
+
+pub fn parse_metadata(front_matter: HashMap<String, String>) -> Result<Metadata, String> {
+    let mut front_matter = front_matter.clone();
+    let title = front_matter.remove("title").ok_or("Title is required")?;
+    let description = front_matter
+        .remove("description")
+        .ok_or("Description is required")?;
+    let date_str = front_matter.remove("date").ok_or("Date is required")?;
+    let tags_str = front_matter.remove("tags").ok_or("Tags are required")?;
+
+    let date = NaiveDate::parse_from_str(&date_str, "%Y-%m-%d")
+        .map_err(|e| format!("Invalid date format: {}. Expected YYYY-MM-DD", e))?;
+
+    let tags = tags_str
+        .split(',')
+        .map(|tag| tag.trim().to_string())
+        .collect::<Vec<String>>();
+
+    Ok(Metadata {
+        title,
+        description,
+        date,
+        tags,
+        extras: front_matter,
     })
 }
 
@@ -158,5 +185,75 @@ mod tests {
         let front_matter = vec![];
         let front_matter = parse_front_matter(front_matter).unwrap();
         assert_eq!(front_matter.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_metadata_ok() {
+        let front_matter = HashMap::from([
+            ("title".to_string(), "My First Post".to_string()),
+            ("date".to_string(), "2021-01-01".to_string()),
+            (
+                "description".to_string(),
+                "This is my first post".to_string(),
+            ),
+            ("tags".to_string(), "tag1,tag2".to_string()),
+        ]);
+
+        let metadata = parse_metadata(front_matter).unwrap();
+        assert_eq!(metadata.title, "My First Post");
+        assert_eq!(metadata.date, NaiveDate::from_ymd_opt(2021, 1, 1).unwrap());
+        assert_eq!(metadata.description, "This is my first post");
+        assert_eq!(metadata.tags, vec!["tag1", "tag2"]);
+        assert_eq!(metadata.extras.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_metadata_ok_extras() {
+        let front_matter = HashMap::from([
+            ("title".to_string(), "My First Post".to_string()),
+            ("date".to_string(), "2021-01-01".to_string()),
+            (
+                "description".to_string(),
+                "This is my first post".to_string(),
+            ),
+            ("tags".to_string(), "tag1,tag2".to_string()),
+            ("hello".to_string(), "world".to_string()),
+            ("foo".to_string(), "bar".to_string()),
+        ]);
+
+        let metadata = parse_metadata(front_matter).unwrap();
+        assert_eq!(metadata.title, "My First Post");
+        assert_eq!(metadata.date, NaiveDate::from_ymd_opt(2021, 1, 1).unwrap());
+        assert_eq!(metadata.description, "This is my first post");
+        assert_eq!(metadata.tags, vec!["tag1", "tag2"]);
+        assert_eq!(metadata.extras.len(), 2);
+        assert_eq!(metadata.extras.get("hello"), Some(&"world".to_string()));
+        assert_eq!(metadata.extras.get("foo"), Some(&"bar".to_string()));
+    }
+
+    #[test]
+    fn test_parse_metadata_invalid_date() {
+        let front_matter = HashMap::from([
+            ("title".to_string(), "My First Post".to_string()),
+            ("date".to_string(), "invalid-date".to_string()),
+            (
+                "description".to_string(),
+                "This is my first post".to_string(),
+            ),
+            ("tags".to_string(), "tag1,tag2".to_string()),
+        ]);
+
+        let result = parse_metadata(front_matter);
+        assert!(result.is_err());
+        assert!(result.err().unwrap().contains("Invalid date format"));
+    }
+
+    #[test]
+    fn test_parse_metadata_missing_required_fields() {
+        let front_matter = HashMap::from([("title".to_string(), "My First Post".to_string())]);
+
+        let result = parse_metadata(front_matter);
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), "Description is required");
     }
 }
--- src/entities.rs
+++ src/entities.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use std::collections::HashMap;
 
 pub enum ContentKind {
@@ -9,4 +10,18 @@ pub struct RawPost {
     pub body: String,
     pub og_path: String,
     pub kind: ContentKind,
+}
+
+pub struct Metadata {
+    pub title: String,
+    pub description: String,
+    pub date: NaiveDate,
+    pub tags: Vec<String>,
+    pub extras: HashMap<String, String>,
+}
+
+pub struct PostOutput {
+    pub route: String,
+    pub metadata: Metadata,
+    pub rendered_content: String,
 }
