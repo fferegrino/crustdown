68b10f0c3ae20df7d7c229f8f00d8fa3245c872d
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-05-18 16:16:48 +0100

    Update tests

---
+++ tests/cli_test.rs
@@ -0,0 +1,52 @@
+use anyhow::Result;
+use assert_cmd::Command;
+use pretty_assertions::assert_eq;
+use std::fs;
+use tempfile::TempDir;
+
+static INPUT_POST_LOCATION: &str = "tests/file_fixtures/inputs/_posts";
+static OUTPUT_POST_LOCATION: &str = "tests/file_fixtures/outputs";
+
+fn compare_files(file1: &str, file2: &str) -> Result<()> {
+    let file1_content = fs::read_to_string(file1)?;
+    let file2_content = fs::read_to_string(file2)?;
+    assert_eq!(file1_content, file2_content, "{} != {}", file1, file2);
+    Ok(())
+}
+
+fn compare_dirs(ground_truth: &str, output: &str) -> Result<()> {
+    let mut ground_truth_files = fs::read_dir(ground_truth)?;
+    while let Some(file) = ground_truth_files.next() {
+        let file = file?;
+        let path = file.path();
+        let file_to_compare = format!("{}/{}", output, path.file_name().unwrap().to_str().unwrap());
+        if path.is_dir() {
+            compare_dirs(&path.to_str().unwrap(), &file_to_compare)?;
+        } else {
+            compare_files(&path.to_str().unwrap(), &file_to_compare)?;
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn test_basic_site_generation() -> Result<()> {
+    let temp_dir = TempDir::new()?;
+    let output_dir = temp_dir.path().join("_site");
+
+    let mut cmd = Command::cargo_bin("crustdown")?;
+
+    let result = cmd
+        .arg("--posts-dir")
+        .arg(INPUT_POST_LOCATION)
+        .arg("--output-dir")
+        .arg(&output_dir)
+        .assert()
+        .success();
+
+    result.success();
+
+    compare_dirs(OUTPUT_POST_LOCATION, output_dir.to_str().unwrap())?;
+
+    Ok(())
+}
